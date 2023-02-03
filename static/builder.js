420c91bf07748131c0d2f8f5bb98470164a2cfd6
margorjon <margorjon@gmail.com>
2023-02-03 20:30:37 +0100

    Update builder.js

--- static/builder.js
+++ static/builder.js
@@ -4570,7 +4570,7 @@ function ensureInitUnitWithSkills(unitId) {
         if (unitsWithSkills[unitId]) {
             resolve(unitsWithSkills[unitId]);
         } else {
-            $.get(`/${server}/unit/${unitId}`, function(result) {
+            $.get(`/static/${server}/unit/${unitId}`, function(result) {
                 unitsWithSkills[unitId] = result;
                 if (units[unitId].braveShift) {
                     ensureInitUnitWithSkills(units[unitId].braveShift).then(() => {
